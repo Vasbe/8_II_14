9eaf41619588f49c0858c691ebb093c4e8496e5d
Vasbe <vasbeme@gmail.com>
2021-11-11 13:31:36 +0400

    Добавьте файлы проекта.

---
+++ 8_II_14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+namespace _8_II_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            StringBuilder s = new StringBuilder(Console.ReadLine());
+            for (int i = 0; i < s.Length; i++) 
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    s.Remove(i, 1);
+                }
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
